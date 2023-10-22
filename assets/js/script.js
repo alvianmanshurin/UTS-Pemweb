9844ed21523143d62a455d837efca1b961886098
AlvianMan <114283162+alvianmanshurin@users.noreply.github.com>
2023-10-22 21:03:20 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const dataForm = document.getElementById("data-form");
-    const dataTable = document.getElementById("data-table").querySelector("tbody");
-    const submitButton = document.getElementById("submit-button");
+    const urlParams = new URLSearchParams(window.location.search);
+    const nama = urlParams.get("nama");
+    const nim = urlParams.get("nim");
+    const prodi = urlParams.get("prodi");
+    const email = urlParams.get("email");
+    const password = urlParams.get("password");
+    const jk = urlParams.get("jk");
+    const tgl_lahir = urlParams.get("tgl_lahir");
+    const alamat = urlParams.get("alamat");
 
-    submitButton.addEventListener("click", function () {
-        const nama = document.getElementById("nama").value;
-        const nim = document.getElementById("nim").value;
-        const prodi = document.getElementById("prodi").value;
-        const email = document.getElementById("email").value;
-
-        // Buat baris data baru untuk tabel
-        const newRow = dataTable.insertRow(dataTable.rows.length);
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
-        cell1.innerHTML = nama;
-        cell2.innerHTML = nim;
-        cell3.innerHTML = prodi;
-        cell4.innerHTML = email;
-
-        // Reset form
-        dataForm.reset();
-    });
+    if (nama) {
+        const table = document.getElementById("registration-table").getElementsByTagName('tbody')[0];
+        const newRow = table.insertRow(table.rows.length);
+        const cells = [nama, nim, prodi, email, password, jk, tgl_lahir, alamat];
+        for (let i = 0; i < cells.length; i++) {
+            newRow.insertCell(i).textContent = cells[i];
+        }
+    }
 });
